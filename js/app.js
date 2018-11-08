75ada8836dc2a1eae85f8fb3a731fdd2b6f4e0d1
anallely753 <anallely.0710@gmail.com>
2018-11-08 00:03:45 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+ M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
   });
+ 
+ var options = {
+  data: {
+    "Java": Javascript,
+    "Javascript": null,
+    "CSS": null,
+    "HTML": null,
+    "NodeJS": null,
+    "PHP": "logo-php.png"
+  },
+}
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
