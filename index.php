130ca442958faab259de6eaa9d73378ff3def4f1
anallely753 <anallely.0710@gmail.com>
2018-11-08 12:39:11 -0600

    Pagina inicial de registro

--- index.php
+++ index.php
@@ -57,88 +57,160 @@ require('conexion.php');
 
 	?>
  <!-- Esta parte creo que se puede separar en php y html, pero no logreé que funcionará -->
-	<!DOCTYPE html>
+<!DOCTYPE html>
 	<html lang="es">
 	<head>
 		<meta charset="UTF-8">
 		<meta  name = "viewport" content="width=device-width,initial-scale=1.0"/>
-		<title>Inicia sesión</title>
+		<title>Inicio</title>
 		<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+		<link rel="stylesheet" href="style.css">
+		 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		 <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+
+		 <!--GOOGLE FONTS-->
+		 <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet"> 
+		 <link href="https://fonts.googleapis.com/css?family=Acme|Dosis" rel="stylesheet"> 
 
 	</head>
 	<body>
 		<header>
+		<!--Barra de anuncio pequeño-->
+		<div class="row anuncio hide-on-small-only"><p>Aprovecha envío gratis en compras mayores a $800</p></div>
 
-			<nav class="nav-extended amber darken-1">
-				<div class="nav-wrapper">
-					<a href="#" class="brand-logo">Logo</a>
-					<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">Menú</i></a>
-					<ul id="nav-mobile" class="right hide-on-med-and-down">
-						<li><a href="index.php">Inicio</a></li>
-						<li><a href="carrito.html">Carrito de compra</a></li>
+		<!--NavBar-->
+		<nav class="nav-extended  z-depth-3 ">
+				<div class="nav-wrapper show-on-medium-and-up hide-on-small-only">
+					
+					<ul class="left">
 						<li><a href="conocenos.html">Conócenos</a></li>
 						<li><a href="contacto.html">Contacto</a></li>
-
+					</ul>
+					<a href="index.html" class="brand-logo center"><img class="img-responsive" src="img/theaka.png" height="60" width="auto"></a>
+					<ul class="right">
+						<li><a href="contacto.html">Productos</a></li>
+						<li><a href="cart.html"><i class="large material-icons">shopping_cart</i></a></li>
 					</ul>
 				</div>
+		</nav>
 
-			</nav>
+		<!--Mobile Navbar-->
+			<div class="navbarsmall">
+			<div class="nav-wrapper hide-on-med-and-up">
+				<img class="img-responsive logo1 center" src="img/theaka.png" >
+				<a href="#" data-target="slide-out" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
+					
+				<ul id="slide-out" class="sidenav">
+				    <li><div class="user-view">
+				    	<div class="row">
+				    		<div class="col s6">
+				    	<img class="img-responsive logo1 center" src="img/theaka.png" >
+				    	</div>
+				    	<div class="col s6 right">
+				    		<div class="row">
+				    		<div class="col s4">	</div>
+				    		<div class="col s4">	</div>
+						    	<div class="col s4 right">
+						    		<a href="cart.html"><i class=" waves-effect small material-icons">shopping_cart</i></a>
+						    	
+						    	</div>
+						    </div>
+				    	
+				    	</div>
+				   		</div>
 
-			<ul class="sidenav" id="mobile-demo">
-				<li><a href="index.php">Inicio</a></li>
-				<li><a href="carrito.html">Carrito de compra</a></li>
-				<li><a href="conocenos.html">Conócenos</a></li>
-				<li><a href="contacto.html">Contacto</a></li>
+				    </div></li>
+				   
+				    <li><a class="waves-effect" href="contacto.html">Contacto</a></li>
+				    <li><a class="waves-effect" href="conocenos.html">Conócenos</a></li>
 
-			</ul>
+				    <li><div class="divider"></div></li>
+				</ul>
+
+
+			</div>
+			</div>
 		</header>
 
-		<h1>Bienvenido a AKA, la mejor tienda online.</h1>
-		<h3>El primer paso es iniciar sesión, si no tienes una, regístrate.</h3>
-		<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+		<!--Foto de supermercado-->
+ 		<div class="row foto">
+      	<div class="col s12"><img class="responsive-img" src="img/super.jpeg"></div>
+  		</div>
 
-			<div>
-				<label> Usuario: </label>
-				<input id="usuario" name="usuario" type="text">
-			</div>
-			<br>
-			<div><label>Contraseña: </label>
-				<input id="password" name="password" type="password">
-			</div>
-			<br>
-			<div>
-				<input name="login" value="Iniciar Sesión" type="submit">
-			</div>
-			<br>
-		</form>
-		<a href="registro.php">Registrarse</a>
-		<div style="font-size: 16px;color: #cc0000;">
-			<?php echo isset($error)?utf8_decode($error):''; ?>
-		</div>
-		<footer class="page-footer amber darken-1">
-			<div class="container">
+  		<div class="row recomendados">	<div class="col s12"><h5>Regístrate para poder comprar</h5></div></div><br>
+
+  		<div class="largo">
+  		<a class="waves-effect waves-light btn-large" href="ingresa.html">Si ya tienes una cuenta da click aqui</a>
+  		</div>
+
+  		<div class="container">
+  		<div class="row">
+  			<br><br>
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          
+<a class="waves-effect waves-light btn">Registrarme</a>
+        </div>
+      </div>
+    </form>
+  </div>
+  </div>
+        
+  		
+		
+		<footer class="page-footer z-depth-3">
 				<div class="row">
-					<div class="col l6 s12">
-						<h5 class="white-text">Footer Content</h5>
-						<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-					</div>
-					<div class="col l4 offset-l2 s12">
-						<h5 class="white-text">Síguemos</h5>
+					<div class="col l4 s12">
+						<h6 class="white-text">Síguenos</h6>
 						<ul>
-							<a class="github-button" href="https://github.com/amairanysolanamejia" aria-label="Follow @amairanysolanamejia on GitHub">Follow @amairanysolanamejia</a>							<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+							<a class="github-button" href="https://github.com/amairanysolanamejia" aria-label="Follow @amairanysolanamejia on GitHub">Follow @amairanysolanamejia</a><br>
+							<a class="github-button" href="https://github.com/anallely753" aria-label="Follow @amairanysolanamejia on GitHub">Follow @anallely753</a><br>
+							<!-- Place this tag where you want the button to render. -->
+<a class="github-button" href="https://github.com/KarinaFloG" aria-label="Follow @KarinaFloG on GitHub">Follow @KarinaFloG</a>
 						</ul>
 					</div>
+					<div class="col l4 s12">
+						<div class="row pagos">
+							<div class="col s3"><img class="responsive-img" src="img/mastercard.png" height="30" width="auto"></div>				
+							<div class="col s3"><img src="img/visa.png" height="30" width="auto"></div>
+							<div class="col s3"><img src="img/paypal.png" height="30" width="auto"></div>
+						</div>						
+					</div>
+					<div class="col l4 s12 ayuda">
+						<h6 class="white-text">¿Necesitas ayuda?</h6>
+						<p class="white-text">Llámanos al 5134.0034<br>
+						o al 01800.367.8737</p>
+					</div>
 				</div>
-			</div>
-			<div class="footer-copyright">
-				<div class="container">
-					© 2014 Copyright Text
-					<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-				</div>
+			<div class="row todos center white-text">
+					 Todos los derechos reservados  |  The AKA Store ® 2018 
 			</div>
 		</footer>
+
 	</body>
 	<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
